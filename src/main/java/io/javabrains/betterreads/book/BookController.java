cecfa9e7b28a7dd679dcd24d8ac4793242281e83
AvdhootJadhav <avdhootjadhav002@gmail.com>
2022-02-19 14:16:51 +0530

    Add Book page and Controller

---
+++ src/main/java/io/javabrains/betterreads/book/BookController.java
@@ -0,0 +1,33 @@
+package io.javabrains.betterreads.book;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class BookController {
+    
+    private final String COVER_IMAGE = "https://covers.openlibrary.org/b/id/";
+    @Autowired BookRepository bookRepository;
+
+    @GetMapping(value = "/books/{bookId}")
+    public String getBook(@PathVariable String bookId, Model model){
+        Optional<Book> optionalBook = bookRepository.findById(bookId);
+        if(optionalBook.isPresent()){
+            Book book = optionalBook.get();
+            String coverImageUrl="src/main/resources/static/images/no-image.png";
+            if(book.getCoverIds() != null && book.getCoverIds().size()>0){
+                coverImageUrl = COVER_IMAGE + book.getCoverIds().get(0) + "-L.jpg";
+                model.addAttribute("coverImage", coverImageUrl);
+            }
+            model.addAttribute("coverImage", coverImageUrl);
+            model.addAttribute("book", book);
+            return "book";
+        }
+        return "book-not-found";
+    }
+}
